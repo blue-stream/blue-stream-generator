b8b533f17b52ef3f199256ede5c026c0bbd06122
Ron Borysovski <roni537@gmail.com>
2018-07-18 17:07:52 +0300

    refactor(questions): Convert to class and use repo-branches as templates

--- src/questions.ts
+++ src/questions.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { config } from './config';
+import { GitService } from './git/git.service';
 
 const CHOICES = fs.readdirSync(config.templatePath);
 
@@ -29,3 +30,36 @@ export const QUESTIONS = [
         }
     }
 ];
+
+export class Questions {
+    static async getQuestions() {
+        const templates: string[] = await GitService.getTemplates();
+
+        return [
+            {
+                name: 'project-template',
+                type: 'list',
+                message: 'What project template would you like to generate?',
+                choices: templates
+            },
+            {
+                name: 'project-name',
+                type: 'input',
+                message: 'Project name:',
+                validate: function (input: string) {
+                    if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+                    else return 'Project name may only include letters, numbers, underscores and hashes.';
+                }
+            },
+            {
+                name: 'main-feature-name',
+                type: 'input',
+                message: 'Main feature name:',
+                validate: function (input: string) {
+                    if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+                    else return 'Feature name may only include letters, numbers, underscores and hashes.';
+                }
+            }
+        ];
+    }
+}
