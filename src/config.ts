9a3ed775452516c6229c4e16db50938202644441
Ron Borysovski <roni537@gmail.com>
2018-07-18 17:07:00 +0300

    chore: Add git config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,10 @@
 export const config = {
     templatePath: 'C:/templates',
-    
+    git: {
+        repo_url: 'https://gitlab.com/sapir-optimizations/blue-generator/microservice-template.git',
+        credentials: {
+            username: 'gitlab+deploy-token-7655',
+            password: 'w_dMsb15gXyiPDgJgiyo'
+        }
+    }
 }
