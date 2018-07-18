fec724514117988186bfe5218fa013d9819e4d2e
Ron Borysovski <roni537@gmail.com>
2018-07-18 17:09:33 +0300

    refactor: Use git service

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import { prompt, Answers } from 'inquirer';
 import { FileUtil } from './utils/file';
 import * as fs from 'fs';
 import { config } from './config';
-import { QUESTIONS } from './questions';
+import { Questions } from './questions';
 
 const CURR_DIR = process.cwd();
 
-prompt(QUESTIONS)
-    .then((answers: Answers) => {
-        const projectChoice = answers['project-choice'];
+Questions.getQuestions().then((questions) => {
+    prompt(questions).then((answers: Answers) => {
+        const projectTemplate = answers['project-template'];
         const projectName = answers['project-name'];
         const mainFeatureName = answers['main-feature-name'];
-        const templatePath = `${config.templatePath}/${projectChoice}`;
+        const templatePath = `${CURR_DIR}/tmp`;
 
         fs.mkdirSync(`${CURR_DIR}/${projectName}`);
 
         FileUtil.createDirectoryContents(templatePath, projectName, mainFeatureName);
     });
-
+});
