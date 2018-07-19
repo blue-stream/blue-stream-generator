ab2d7190012dbc0c45e40cc7978f35643a9a387c
Ron Borysovski <roni537@gmail.com>
2018-07-19 12:32:22 +0300

    chore(config): Add temp dir config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
     templatePath: 'C:/templates',
+    temporaryDirectoryPath: `${process.cwd()}/tmp`,
     git: {
         repo_url: 'https://gitlab.com/sapir-optimizations/blue-generator/microservice-template.git',
         credentials: {
