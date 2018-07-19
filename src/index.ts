4b6352af8a3327eec10873de3787f27a696e59da
Ron Borysovski <roni537@gmail.com>
2018-07-19 12:33:25 +0300

    style: Change promise to async function

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import { prompt, Answers } from 'inquirer';
 import { FileUtil } from './utils/file';
-import * as fs from 'fs';
 import { config } from './config';
 import { Questions } from './questions';
+import { Generator } from './generator';
 
 const CURR_DIR = process.cwd();
 
-Questions.getQuestions().then((questions) => {
-    prompt(questions).then((answers: Answers) => {
-        const projectTemplate = answers['project-template'];
-        const projectName = answers['project-name'];
-        const mainFeatureName = answers['main-feature-name'];
-        const templatePath = `${CURR_DIR}/tmp`;
+(async () => {
+    const questions = await Questions.getQuestions();
+    const answers: Answers = await prompt(questions);
+    const template = answers['project-template'];
+    const name = answers['project-name'];
+    const featureName = answers['main-feature-name'];
+    await Generator.generateTemplate(template, name, featureName);
+})();
 
-        fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+// Questions.getQuestions().then((questions) => {
+//     prompt(questions).then((answers: Answers) => {
+//         const projectTemplate = answers['project-template'];
+//         const projectName = answers['project-name'];
+//         const mainFeatureName = answers['main-feature-name'];
+//         const templatePath = `${CURR_DIR}/tmp`;
 
-        FileUtil.createDirectoryContents(templatePath, projectName, mainFeatureName);
-    });
-});
+//         fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+//         FileUtil.createDirectoryContents(templatePath, projectName, mainFeatureName);
+
+//         return Generator.generateTemplate(projectTemplate, projectName, mainFeatureName);
+//     });
+// });
