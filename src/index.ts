ff3f70194b019e29d5cfaa5a908e96caa330aa50
Ron Borysovski <roni537@gmail.com>
2018-07-22 13:49:15 +0300

    chore: Add text indicators to generator

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
 import { prompt, Answers } from 'inquirer';
-import { FileUtil } from './utils/file';
-import { config } from './config';
-import { Questions } from './questions';
-import { Generator } from './generator';
+import * as chalk from 'chalk';
+import * as Ora from 'ora';
+const figlet = require('figlet');
 
-const CURR_DIR = process.cwd();
+import { Generator } from './generator';
+import { Questions } from './questions';
 
 (async () => {
+    console.log(
+        chalk.default.blue(
+            figlet.textSync('Generator', {
+                font: 'colossal'
+            })
+        ),
+        chalk.default.grey('by Ron Borysovski')
+    );
+
+    const templates = new Ora({
+        text: 'Fetching templates from git repository',
+        color: 'blue'
+    }).start();
+
     const questions = await Questions.getQuestions();
+
+    templates.succeed();
+
     const answers: Answers = await prompt(questions);
     const template = answers['project-template'];
     const name = answers['project-name'];
     const featureName = answers['main-feature-name'];
+
+    const generator = new Ora({
+        text: 'Generating template files',
+        color: 'blue'
+    }).start();
+
     await Generator.generateTemplate(template, name, featureName);
+
+    generator.succeed();
 })();
-
-// Questions.getQuestions().then((questions) => {
-//     prompt(questions).then((answers: Answers) => {
-//         const projectTemplate = answers['project-template'];
-//         const projectName = answers['project-name'];
-//         const mainFeatureName = answers['main-feature-name'];
-//         const templatePath = `${CURR_DIR}/tmp`;
-
-//         fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-
-//         FileUtil.createDirectoryContents(templatePath, projectName, mainFeatureName);
-
-//         return Generator.generateTemplate(projectTemplate, projectName, mainFeatureName);
-//     });
-// });
