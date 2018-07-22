4028beacbb13c360126664a3ab3bd4705c682bda
Ron Borysovski <roni537@gmail.com>
2018-07-22 13:48:11 +0300

    chore: Add rootDir and generatorId to config files

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
+import * as uniqid from 'uniqid';
+
 export const config = {
-    templatePath: 'C:/templates',
-    temporaryDirectoryPath: `${process.cwd()}/tmp`,
+    rootDir: process.cwd(),
+    generatorId: uniqid(),
     git: {
         repo_url: 'https://gitlab.com/sapir-optimizations/blue-generator/microservice-template.git',
         credentials: {
