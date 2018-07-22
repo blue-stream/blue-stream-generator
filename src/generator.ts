e3a4a1ec09d8a82ee8e010eacfe7c83dfbe6c15a
Ron Borysovski <roni537@gmail.com>
2018-07-22 13:48:30 +0300

    chore: Remove temp files after generation

--- src/generator.ts
+++ src/generator.ts
@@ -1,7 +1,20 @@
-import { GitService } from "./git/git.service";
+import { copy, remove } from 'fs-extra';
+import { config } from './config';
+import { GitService } from './git/git.service';
 
 export class Generator {
+    /**
+     * Generate project template from specific template name
+     * @param template Template name
+     * @param name Project name
+     * @param featureName Main feature name
+     */
     static async generateTemplate(template: string, name: string, featureName: string) {
-        await GitService.cloneBranch(`template-${template}`);
+        const cloneTempPath = `${config.rootDir}/clone-${config.generatorId}/`;
+
+        await GitService.cloneBranch(`template-${template}`, cloneTempPath);
+
+        await copy(cloneTempPath, `${config.rootDir}/${name}`);
+        await remove(cloneTempPath);
     }
 }
