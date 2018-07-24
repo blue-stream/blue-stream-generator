13350e88c63ee21284a364e08d2d6cdf9e849cae
Ron Borysovski <roni537@gmail.com>
2018-07-24 10:13:06 +0300

    feat: Replace template names after cloning git repository

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,6 @@
-import { copy, remove } from 'fs-extra';
 import { config } from './config';
-import { GitService } from './git/git.service';
+import { GitService } from './git.service';
+import { ReplacerUtil } from './util/replacer';
 
 export class Generator {
     /**
@@ -8,13 +8,24 @@ export class Generator {
      * @param template Template name
      * @param name Project name
      * @param featureName Main feature name
+     * @example
+     * generateTemplate('with-db', 'users-microservice', 'user')
+     * // Will generate a project from the templates repository in the following structure
+     * // users-microservice/
+     * //  |___ src/
+     * //       |___ users/
+     * //       |      |_________ users.controller.ts
+     * //       |      |_________ users.router.ts
+     * //       |      :
+     * //       |      |_________ users.interface.ts
+     * //       |___ config.ts
+     * //       |___ index.ts
+     * //       :
+     * //       |___ server.ts
      */
     static async generateTemplate(template: string, name: string, featureName: string) {
-        const cloneTempPath = `${config.rootDir}/clone-${config.generatorId}/`;
-
-        await GitService.cloneBranch(`template-${template}`, cloneTempPath);
-
-        await copy(cloneTempPath, `${config.rootDir}/${name}`);
-        await remove(cloneTempPath);
+        await GitService.cloneBranch(`template-${template}`, `${config.rootDir}/${name}`);
+        ReplacerUtil.replaceFileNames(`${config.rootDir}/${name}`, featureName);
+        ReplacerUtil.replaceFileContents(`${config.rootDir}/${name}`, featureName);
     }
 }
