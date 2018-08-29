e70e39fc033bb94ebaa0c3843928f80bee37e4d2
Ron Borysovski <roni537@gmail.com>
2018-08-29 11:19:26 +0300

    feat: Template util to modify original files

--- src/util/template.ts
+++ src/util/template.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
+import { config } from '../config';
 
 export class Template {
+    
     static fetchFeatures(path: string, features: string[] = []) {
         const files: string[] = fs.readdirSync(`${path}`);
         files.forEach((file: string) => {
@@ -22,26 +24,51 @@ export class Template {
         return [...new Set(features)];
     }
 
-    static removeFeature(path: string, feature: string) {
-        const files: string[] = fs.readdirSync(`${path}`);
-        files.forEach((file: string) => {
-            const stats = fs.statSync(`${path}/${file}`);
-            if (file !== '.git') {
+
+    private static getFeatureRegex(feature: string): RegExp {
+        const featurePattern = `\\/\\/\\s*<${feature}>([\\s\\S]*?)\\/\\/\\s*<\\/${feature}>`;
+        return new RegExp(featurePattern, 'mg');
+    }
+
+    private static getInvertedFeatureRegex(feature: string): RegExp {
+        const invertedFeaturePattern = `\\/\\/\\s*!<${feature}>([\\s\\S]*?)\\/\\/\\s*!<\\/${feature}>`;
+        return new RegExp(invertedFeaturePattern, 'mg');
+    }
+
+    static applyFeatures(path: string, featuresToDisable: string[], allFeatures: string[]) {
+        const files: string[] = fs.readdirSync(path);
+        files.forEach(file => {
+            if (config.ignoreDirectories.indexOf(file) === -1) {
+                const stats = fs.statSync(`${path}/${file}`);
                 if (stats.isFile()) {
                     const fileContent = fs.readFileSync(`${path}/${file}`, 'utf8');
-                    const regexPattern = `\\/\\/\\s*<${feature}>([\\s\\S]*?)\\/\\/\\s*<\\/${feature}>`;
-                    const removeRegex = new RegExp(regexPattern, 'mg');
-                    console.log(removeRegex)
-                    const replacedFile = fileContent.replace(removeRegex, '');
-                    if (replacedFile != fileContent) {
-                        console.log(replacedFile);
-                        fs.writeFileSync(`${path}/${file}`, replacedFile, 'utf8');
+                    let modifiedContent = fileContent;
+                    allFeatures.forEach(feature => {
+                        const shouldRemove = featuresToDisable.includes(feature);
+                        const removeRegex = shouldRemove ?
+                            Template.getFeatureRegex(feature) :
+                            Template.getInvertedFeatureRegex(feature);
+
+                        const unwrapRegex = shouldRemove ?
+                            Template.getInvertedFeatureRegex(feature) :
+                            Template.getFeatureRegex(feature);
+
+                        modifiedContent = modifiedContent.replace(removeRegex, '');
+                        modifiedContent = modifiedContent.replace(unwrapRegex, '$1');
+                        const removeSuffixPattern = `_REMOVE\\/\\*${feature}\\*\\/`;
+                        modifiedContent = modifiedContent.replace(new RegExp(removeSuffixPattern, 'mg'), '');
+                    });
+                    if (modifiedContent !== fileContent) {
+                        if (modifiedContent.trim()) {
+                            fs.writeFileSync(`${path}/${file}`, modifiedContent, 'utf8');
+                        } else {
+                            fs.unlinkSync(`${path}/${file}`);
+                        }
                     }
                 } else if (stats.isDirectory()) {
-                    Template.removeFeature(`${path}/${file}`, feature);
+                    Template.applyFeatures(`${path}/${file}`, featuresToDisable, allFeatures);
                 }
             }
         });
-
     }
 }
