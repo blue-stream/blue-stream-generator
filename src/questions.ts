d956fe11b327d77c5b376c0dfdca762ce48890f2
Ron Borysovski <roni537@gmail.com>
2018-08-29 11:17:11 +0300

    refactor: Each question is now independent

--- src/questions.ts
+++ src/questions.ts
@@ -1,34 +1,46 @@
-import { GitExecuter } from './git';
+import { prompt, Answers } from 'inquirer';
 
 export class Questions {
-    static async getQuestions() {
-        const templates: string[] = await GitExecuter.getBranchList();
 
-        return [
-            {
-                name: 'project-template',
-                type: 'list',
-                message: 'What project template would you like to generate?',
-                choices: templates
-            },
-            {
-                name: 'project-name',
-                type: 'input',
-                message: 'Project name:',
-                validate: function (input: string) {
-                    if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-                    else return 'Project name may only include letters, numbers, underscores and hashes.';
-                }
-            },
-            {
-                name: 'main-feature-name',
-                type: 'input',
-                message: `Main feature name:`,
-                validate: function (input: string) {
-                    if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-                    else return 'Feature name may only include letters, numbers, underscores and hashes.';
-                }
+    static async getProjectName(): Promise<string> {
+        const answer: Answers = await prompt([{
+            name: 'project-name',
+            type: 'input',
+            message: 'Project name',
+            validate: function (input: string) {
+                if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+                else return 'Project name may only include letters, numbers, underscores and hashes.';
             }
-        ];
+        }]);
+
+        return answer['project-name'];
+    }
+
+    static async getMainFeatureName(): Promise<string> {
+        const answer: Answers = await prompt([{
+            name: 'main-feature',
+            type: 'input',
+            message: 'Main feature name',
+            validate: function (input: string) {
+                if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+                else return 'Feature name may only include letters, numbers, underscores and hashes.';
+            }
+        }]);
+
+        return answer['main-feature'];
+    }
+
+    static async getSelectedFeatures(features: string[]): Promise<string[]> {
+        const answer: Answers = await prompt([{
+            type: 'checkbox',
+            message: 'Select features to enable',
+            name: 'selected-features',
+            choices: features.map(feature => ({
+                name: feature,
+                checked: true
+            })),
+        }]);
+
+        return answer['selected-features'];
     }
 }
